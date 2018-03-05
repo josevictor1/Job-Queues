c18f5e5dd0d2e7749d5850d3cb575b9f241e11da
José Victor <josevictorpatos@gmail.com>
2018-03-04 23:19:17 -0300

    done

--- src/theproject/outline/core.clj
+++ src/theproject/outline/core.clj
@@ -1,74 +1,47 @@
 (ns theproject.core
   (:gen-class)
-  (:require [cheshire.core :refer :all])
-  (:require [datomic.api :as d]))
-
-(defn read-json-data [archivepath] 
-  (parse-string (slurp archivepath) true))
-
-(def datareceived (vec (read-json-data "resource/teste.json")))
+  (:require [cheshire.core :refer :all]))   
+;Reading of the resource directory:  
+;(defn read-json-data [archivepath] (parse-string (slurp archivepath) true))
+;(def datareceived (vec (read-json-data "resource/teste.json")))
 
 (defn get-agents [coll] (filter #(:new_agent %) coll))
 (defn get-jobs [coll] (filter #(:new_job %) coll))
 (defn get-jobs-requested [coll] (filter #(:job_request %) coll))
-
-(defn get-jobs-unu [coll boll] 
-  (filter #(= (:urgent (:new_job %)) boll) coll))
+(defn get-jobs-unu [coll boll] (filter #(= (:urgent (:new_job %)) boll) coll))
 
 (defn select-available-agent [coll_jobs_requested coll_agents agents_list] 
   (cond 
     (or (empty? coll_jobs_requested) (empty? coll_agents)) 
       agents_list
-    (= (str (:agent_id (:job_request (first coll_jobs_requested)))) (:id (:new_agent (first coll_agents)))) 
-      (do
-        (println (= (:agent_id (:job_request (first coll_jobs_requested)) (:id (:new_agent (first coll_agents))))))
-        (println " entrou" (:agent_id (:job_request (first coll_jobs_requested))))
-        (println "segundo" (:id (:new_agent (first coll_agents))))
-        (recur (rest coll_jobs_requested) (rest coll_agents) (conj agents_list (first coll_agents)))
-        )
-       
+    (= (:agent_id (:job_request (first coll_jobs_requested))) (:id (:new_agent (first coll_agents)))) 
+      (recur (rest coll_jobs_requested) (rest coll_agents) (conj agents_list (first coll_agents))) 
     :else 
       (recur coll_jobs_requested (rest coll_agents) agents_list)))
 
-(def jobs (into (get-jobs-unu datareceived false) (get-jobs-unu datareceived true)))
-(def agents (select-available-agent (get-jobs-requested datareceived) (get-agents datareceived) []))
-
-(defn have-skill? [skill agt-skilllist] 
-  (some? (some #{skill} agt-skilllist)))
-
-(defn filter-agent[job agents skilllv] 
-  (filter #(have-skill? (:type (:new_job  job)) (skilllv (:new_agent  %))) agents))    
-
-(defn remove-agent [agent agentlist] 
-  (remove #(= agent %) agentlist))    
-
-(defn mountjob_assigned [job agent] 
-  (assoc-in {} [:job_assigned] 
-    {:job_id (:id (:new_job job)) :agent_id (:id (:new_agent agent))}))
+(defn have-skill? [skill agt_skilllist] (some? (some #{skill} agt_skilllist)))        
+(defn mountjob_assigned [job agent] (assoc-in {} [:job_assigned] {:job_id (:id (:new_job job)) :agent_id (:id (:new_agent agent))}))
+(defn remove-job [job joblist] (remove #(= job %) joblist))
+(defn filter-job [agt jobs skilllv] (filter #(have-skill? (:type (:new_job  %)) (skilllv (:new_agent  agt))) jobs))
 
 (defn dequeue [coll_jobs coll_agts]
   (loop [jobs coll_jobs agts coll_agts result []]
     (if (or (empty? jobs) (empty? agts)) 
       result
-      (let [primary (filter-agent (first jobs) agts :primary_skillset) secondary (filter-agent (first jobs) agts :secondary_skillset)] 
+      (let [primary (filter-job (first agts) jobs :primary_skillset) secondary (filter-job (first agts) jobs :secondary_skillset)] 
         (cond 
-          (not (empty? primary)) 
-            (recur (rest jobs) (remove-agent (first primary) agts) (conj result (mountjob_assigned (first jobs) (first primary))))
-          (and (empty? primary) (not (empty? primary)))
-            (recur (rest jobs) (remove-agent (first secondary) agts) (conj result (mountjob_assigned (first jobs) (first primary))))
+          (= (empty? primary) false) 
+            (recur (remove-job (first primary) jobs) (rest agts) (conj result (mountjob_assigned (first primary) (first agts))))
+          (and (= (empty? primary) true) (= (empty? secondary) false))
+            (recur (remove-job (first secondary) jobs) (rest agts) (conj result (mountjob_assigned (first secondary) (first agts))))
           :else
-            (recur (rest jobs)  agts result))))))
+            (recur jobs (rest agts) result))))))
 
-(println (dequeue jobs agents))
 (defn -main
   [& args] 
-
-  ; (let [in (slurp *in*)]
-  ;   (println (parse-string in)))
-
-  ;; Reciving data, done!
-  ;(def data-readed (parse-string (slurp *in*)))
-)
-;;Don't use concat https://stuartsierra.com/2015/04/26/clojure-donts-concat
-
-
+  (def datareceived (parse-string (slurp *in*) true))
+  (def jobs (into (get-jobs-unu datareceived false) (get-jobs-unu datareceived true)))
+  (def agents (select-available-agent (get-jobs-requested datareceived) (get-agents datareceived) []))
+  (def result (dequeue jobs agents))
+  (println (generate-string result))
+  (generate-stream result (clojure.java.io/writer "/Users/josevictorpereiracosta/Documents/secret/jobqueue/resource/result.json")))
